04d2f3a3d49e4e1fee8160e3e401ccb8b096270f
SeungHo Lee <seung7642@neowiz.com>
2020-04-04 22:17:38 +0900

    JUnit : @Test 실행해보기.

---
+++ chap02/src/test/java/application/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void test() {
+        fail("Not yes implemented");
+    }
+}
