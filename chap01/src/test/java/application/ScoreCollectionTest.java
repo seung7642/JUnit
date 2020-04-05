4c2c290b51717eb418349a73b02ddb2e5f8eef9e
SeungHo Lee <seung7642@neowiz.com>
2020-04-05 22:30:50 +0900

    Chap01 : 간단한 테스트 코드 실행해보기.

---
+++ chap01/src/test/java/application/ScoreCollectionTest.java
@@ -0,0 +1,24 @@
+package application;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void answersArithmeticMeanOfTwoNumbers() {
+
+        // Arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // Act
+        int actualResult = collection.arithmeticMean();
+
+        // Assert
+        assertThat(actualResult, equalTo(6));
+    }
+}
